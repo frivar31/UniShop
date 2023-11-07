bd935a3d1d57e914785ca529d8dfbd29589b5279
Olivier Dery Prevost <olivier.dery-prevost@umontreal.ca>
2023-11-07 17:58:37 -0500

    Order update

---
+++ devoir2/Unishop/src/Data/Entities/Order.java
@@ -0,0 +1,83 @@
+package Data.Entities;
+
+import Data.Entities.Products.Product;
+
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+
+    private String orderNumber;
+    private List<Product> items;
+    private Date orderDate;
+    private Boolean delivered;
+    private Boolean shipped;
+    private Date shippedDate;
+    private Date deliveryDate;
+
+    public Order(String orderNumber, List<Product> items, Date orderDate, Boolean delivered, Boolean shipped, Date shippedDate, Date deliveryDate) {
+        this.orderNumber = orderNumber;
+        this.items = items;
+        this.orderDate = orderDate;
+        this.delivered = delivered;
+        this.shipped = shipped;
+        this.shippedDate = shippedDate;
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public List<Product> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Product> items) {
+        this.items = items;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Boolean getDelivered() {
+        return delivered;
+    }
+
+    public void setDelivered(Boolean delivered) {
+        this.delivered = delivered;
+    }
+
+    public Boolean getShipped() {
+        return shipped;
+    }
+
+    public void setShipped(Boolean shipped) {
+        this.shipped = shipped;
+    }
+
+    public Date getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(Date shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+}
