cb3f31cbe1d8e721fc96a437857687383700779b
Olivier Dery Prevost <olivier.dery-prevost@umontreal.ca>
2023-11-09 13:41:39 -0500

    custom toString() for seller and product

--- devoir2/Unishop/src/Data/Entities/Users/Seller.java
+++ devoir2/Unishop/src/Data/Entities/Users/Seller.java
@@ -4,12 +4,43 @@ import Data.Entities.Products.Product;
 
 import java.util.ArrayList;
 
-public class Seller extends User{
+public class Seller extends User {
 
     ArrayList<Product> products;
 
-    public Seller(String firstName, String lastName, String email, String pseudo, Long number) {
+    public Seller(String firstName, String lastName, String email, String pseudo, Long number, ArrayList<Product> productsToSell) {
         super(firstName, lastName, email, pseudo, number);
-        products = new ArrayList<Product>();
+        products = productsToSell;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public boolean deleteProduct(Product product) {
+        try {
+            products.remove(product);
+        } catch (Exception e) {
+            return false;
+        }
+        ;
+        return true;
+    }
+    public Product getProduct(int index){
+        return products.get(index);
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb=new StringBuilder();
+        int index=0;
+        for(Product product:products){
+            sb.append("#").append(index).append(" ").append(product.toString());
+            sb.append("/n");
+        }
+        return sb.toString();
     }
 }
