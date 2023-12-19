4af09a0581a007190482406514357d3a396767f3
frivar <mekki.d.tarek@hotmail.com>
2023-12-19 15:48:24 -0500

    tests

--- Unishop Java Project/src/main/java/tests.java
+++ Unishop Java Project/src/main/java/tests.java
@@ -5,13 +5,11 @@ import static org.mockito.Mockito.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
-import Data.Entities.Catalog;
-import Data.Entities.Order;
-import Data.Entities.OrderItem;
+import Data.Entities.*;
 import Data.Entities.Products.Product;
 import Data.Entities.Products.ProductType;
-import Data.Entities.ShoppingCart;
 import Data.Entities.Users.Client;
 import Data.Entities.Users.Seller;
 import org.junit.Before;
@@ -36,7 +34,11 @@ public class tests {
 
         @Test
         public void buy() {
-            Product product1 = new Product("Laptop",
+            ProductEvaluation evaluation1 = new ProductEvaluation(4, "Great product!", "user1", 1);
+            ProductEvaluation evaluation2 = new ProductEvaluation(5, "Excellent service!", "user2", 2);
+            // Create some example products
+            Product product1 = new Product(
+                    "Laptop",
                     "Powerful laptop with high-end specifications",
                     "2023-01-01",
                     10,
@@ -44,8 +46,13 @@ public class tests {
                     1,
                     ProductType.Hardware,
                     "Model123",
-                    "BrandXYZ");
-            Product product2 = new Product( "Book",
+                    "BrandXYZ",
+                    new ArrayList<>(List.of(evaluation1)),
+                    new ArrayList<>(List.of("user1", "user2"))
+            );
+
+            Product product2 = new Product(
+                    "Book",
                     "Interesting book on a specific topic",
                     "2023-02-15",
                     50,
@@ -53,12 +60,16 @@ public class tests {
                     1,
                     ProductType.Book,
                     "BookModel456",
-                    "BookBrandABC");
+                    "BookBrandABC",
+                    new ArrayList<>(List.of(evaluation2)),
+                    new ArrayList<>(List.of("user3", "user4"))
+            );
 
             // Create an ArrayList of products
             ArrayList<Product> productsList = new ArrayList<>();
             productsList.add(product1);
             productsList.add(product2);
+
 
             // Create a Seller instance
             Seller seller1 = new Seller(
@@ -73,9 +84,23 @@ public class tests {
             client = new Client("John", "Doe", "john@example.com", "johnny", 123456789L, "123 Main St", "password");
             // Créer une commande
 
-            Product product = new Product("Mock Product", "Description", "2023-01-01", 10, 49.99, 1, ProductType.Hardware, "Model123", "B");
+            Product product = new Product(
+                    "Smartphone",
+                    "High-performance smartphone with a sleek design",
+                    "2023-03-10",
+                    20,
+                    699.99,
+                    1,
+                    ProductType.Hardware,
+                    "Model456",
+                    "BrandTech",
+                    new ArrayList<>(),  // No evaluations initially
+                    new ArrayList<>(List.of("user5", "user6"))
+            );
             seller1.addProduct(product);
-
+            System.out.println(seller1.getProduct(1).toString());
+            System.out.println(seller1.getProduct(2).toString());
+/*
             System.out.println(product.toString(1));
             Catalog.catalogMap.put(product.getId(), new Object[]{product, });
             ShoppingCart shoppingCart = new ShoppingCart();
@@ -85,7 +110,7 @@ public class tests {
             System.out.println(product.getPrice());
             //shoppingCart.add(mockProduct.getId());
             System.out.println(shoppingCart.toString());
-
+    */
 
             // Store the initial points of the client
             //int initialPoints = client.getPoints();
