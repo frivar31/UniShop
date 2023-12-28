84a246c41db81b4bf4b380761ce0320552bd93e5
frivar <mekki.d.tarek@hotmail.com>
2023-12-28 00:55:35 -0500

    javadocs

---
+++ Documentation/javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Controller"},{"l":"Data.Entities"},{"l":"Data.Entities.Products"},{"l":"Data.Entities.Users"},{"l":"Service"}];updateSearchResults();
